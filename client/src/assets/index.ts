8fafb8854c452b68faf87bec77d10e6e1d8f9859
Victor Oliveira <victorsilva101@hotmail.com>
2023-12-08 12:31:20 -0300

    Challenge Complete

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,5 +1,4 @@
-import LogoCITi1 from "./Logo1.png";
-import LogoCITi2 from "./Logo2.png";
+import Logociti from "./CitiLogo.svg";
 import Mangue from "./Mangue.png";
 
-export { LogoCITi1, LogoCITi2, Mangue };
+export { Logociti, Mangue };
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,91 @@
 import React from "react";
-import { LogoCITi1 } from "../assets";
+import { Logociti, Mangue } from "../assets";
+import { Button, TextField, Typography, Box, Container } from "@mui/material";
 
 export default function Home() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "50px"}}>
-      <img src={LogoCITi1.src} alt="" style={{ width: "25%"}}/>
-      <h1>Next.js Boilerplate</h1>
-      <p>
-        Made with <strong>&lt; &#x0002F; &gt;</strong> and{" "}
-        <strong>&hearts;</strong> by CITi
-      </p>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <Box
+        style={{
+          display: "flex",
+          flexDirection: "column",
+
+          position: "absolute",
+          left: "50px",
+          marginTop: "50px",
+        }}
+      >
+        <div
+          style={{
+            paddingBottom: "120px",
+          }}
+        >
+          <img src={Logociti.src} alt="Logo" />
+        </div>
+        <Typography
+          style={{ marginBottom: "20px" }}
+          color="#1E293B"
+          variant="h4"
+        >
+          Acesse a plataforma
+        </Typography>
+        <p color="#475569" style={{ marginBottom: "20px" }}>
+          Faça login ou registre-se para começar a construir seus projetos ainda
+          hoje.
+        </p>
+        <Typography color={"#1E293B"}>E-mail</Typography>
+        <TextField
+          type="text"
+          placeholder="Digite seu email"
+          style={{ marginTop: "20px" }}
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            justifyItems: "center",
+          }}
+        >
+          <Typography style={{ marginTop: "20px" }} color={"#1E293B"}>
+            Senha
+          </Typography>
+          <Button variant="text" color="success" style={{ paddingTop: "30px" }}>
+            Esqueceu sua senha?
+          </Button>
+        </div>
+        <TextField
+          type="password"
+          placeholder="Digite sua senha"
+          style={{ marginTop: "5px" }}
+        />
+        <Button
+          variant="contained"
+          color="success"
+          style={{ marginTop: "20px", height: "50px" }}
+        >
+          Entrar
+        </Button>
+      </Box>
+      <div
+        style={{
+          display: "flex",
+          position: "absolute",
+          right: "0",
+          bottom: "0",
+          top: "0",
+        }}
+      >
+        <img src={Mangue.src} alt="Mangue" />
+      </div>
     </div>
   );
 }
